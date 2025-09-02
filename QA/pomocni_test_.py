34ded6c97c2cf072b60418a1278fed6fc7fa9612
Milan Nedić <milannedic.kg@gmail.com>
2025-09-02 20:42:16 +0200

    Test za Registraciju i Logovanje
    Title

--- QA/pomocni_test_.py
+++ QA/pomocni_test_.py
@@ -1,52 +1,6 @@
-import requests
-import uuid
-import mysql.connector
-from argon2 import PasswordHasher
-from argon2.exceptions import VerifyMismatchError
-from pymongo import MongoClient
+from selenium import webdriver
 
-konekcija = mysql.connector.connect(
-    host="localhost",         
-    port=3004,
-    user="pisbp",
-    password="pisbp",
-    database="users"
-)
-
-klijent = MongoClient('mongodb://pisbp:pisbp@localhost:27017/')
-
-databaza = klijent["formsquestionsdatabase"]
-kolekcija = databaza["formscollection"]
-
-sadrzaj = kolekcija.find()
-
-for i in sadrzaj:
-    print(i)
-
-print("\n")
-
-kursor = konekcija.cursor()
-kursor.execute("use users;")
-kursor.execute("select * from users")
-listakorisnika = kursor.fetchall()
-
-for i in listakorisnika:
-    print(i)
-
-#brisanje sadrzaja
-kolekcija.delete_many({})
-kveri = "DELETE FROM users WHERE firstname = %s"
-ime = "testjoe"
-ime2 = "ime"
-ime3 = "kolaborator"
-ime4 = "testjoe1"
-kursor.execute(kveri, (ime,) )
-kursor.execute(kveri, (ime2,) )
-kursor.execute(kveri, (ime3,) )
-kursor.execute(kveri, (ime4,) )
-konekcija.commit()
-
-kursor.close()
-konekcija.close()
-
-#napravi isto ovo za brisanje pitanja
+driver = webdriver.Chrome()  # Selenium Manager se brine o driveru
+driver.get("https://www.google.com")
+print(driver.title)
+driver.quit()
